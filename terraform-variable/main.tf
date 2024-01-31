82827bbff17072d604afca8056490456bef90315
Tauqeer Ahmad <hellotauqeer@gmail.com>
2024-01-31 21:16:37 +0530

    terraform-variable

---
+++ terraform-variable/main.tf
@@ -0,0 +1,13 @@
+resource "local_file" "devops" {
+  filename = var.file_list[0]
+  content = var.content_map["content1"]
+}
+
+resource "local_file" "devops-var" {
+  filename = var.filename
+  content = var.content_map["content2"]
+}  //var is helping us to get the data from variabes.tf, it is being managed by terraform.tfstate
+
+output "aws_ec2_info" {
+  value = var.aws_ec2_object.instances
+}
---
+++ terraform-variable/variables.tf
@@ -0,0 +1,35 @@
+variable "filename" {
+    default = "/home/tauqeerops/terraform-course/terraform-variable/devops-automated.txt"
+}
+
+variable "content" {
+    default = "This a autogenerate from a variable"
+}
+
+variable "content_map" {
+  type=map
+  default = {
+    "content1" = "this is a cool content 1"
+    "content2" = "this is a cooler content 2"
+  }
+}
+
+variable "file_list" {
+  type = list
+  default = ["/home/tauqeerops/terraform-course/terraform-variable/file1.txt","/home/tauqeerops/terraform-course/terraform-variable/file2.txt"]
+}
+
+variable "aws_ec2_object" {
+  type = object({
+    name = string
+    instances = number
+    keys = list(string)
+    ami = string
+  })
+  default = {
+    name = "test_ec2_instances"
+    instances = 4
+    keys = ["key1.pem", "key2.pem"]
+    ami = "ubuntu-aef51"
+  }
+}
