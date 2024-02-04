cac0945a0637153804ac50fb8c5ed1da46b6cdd2
Tauqeer Ahmad <hellotauqeer@gmail.com>
2024-02-04 12:17:37 +0530

    creating instances using diff ami_keypair

--- terraform-aws/variables.tf
+++ terraform-aws/variables.tf
@@ -1,3 +1,3 @@
 locals {
-  instance_name = toset(["Tauqeer", "Anas", "Ameen", "Pablo"])
+  instance_name = {"Tauqeer":"ami-0277155c3f0ab2930", "Anas":"ami-0c7217cdde317cfec", "Ameen":"ami-0fe630eb857a6ec83", "Pablo":"ami-00d990e7e5ece7974"}
 }
